bee8a71ba7a5112d368916b2f659a2ce86034b17
Liam <liam@liamparker.com>
2013-06-04 07:47:19 +1000

    Main code

---
+++ NSString+Email.h
@@ -0,0 +1,14 @@
+//
+//  NSString+Email.h
+//
+//  Created by Liam Parker on 6/02/13.
+//  Copyright (c) 2013 Karma Imperial. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Email)
+
+- (BOOL)isEmail;
+
+@end
