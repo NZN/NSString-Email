46f3eb8fdb8905344fa264aaaf261de55ab325e9
Matthew Wilkinson <matt@somerobots.com>
2016-07-01 22:40:11 +0100

    Carthage support
    Signed-off-by: Matthew Wilkinson <matt@somerobots.com>

---
+++ NSString-Email/NSString_Email.h
@@ -0,0 +1,19 @@
+//
+//  NSString-Email.h
+//  NSString-Email
+//
+//  Created by Matthew Wilkinson on 01/07/2016.
+//  Copyright © 2016 No Zebra Network. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSString-Email.
+FOUNDATION_EXPORT double NSString_EmailVersionNumber;
+
+//! Project version string for NSString-Email.
+FOUNDATION_EXPORT const unsigned char NSString_EmailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSString_Email/PublicHeader.h>
+
+<NSString_Email/NSString+Email.h>
